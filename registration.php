0f30b19788632b331415250ef3914ea502ce0100
Michele Fantetti <mfantetti@ittweb.net>
2020-01-31 11:31:45 +0100

    Extension registration

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WaPoNe
+ *
+ * @category   WaPoNe
+ * @package    WaPoNe_FeedGenerator
+ * @copyright  Copyright (c) 2020 WaPoNe (https://www.fantetti.net)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'WaPoNe_FeedGenerator',
+    __DIR__
+);
